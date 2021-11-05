74a55db14ba86170c780f8f2a1f1dcbb76185374
Sean CC <seanlongcc@gmail.com>
2021-11-05 13:16:33 -0400

    removed button and made gathering process automatic

--- app/src/main/java/com/molehill/android/MainActivity.kt
+++ app/src/main/java/com/molehill/android/MainActivity.kt
@@ -15,55 +15,57 @@ import java.io.Writer
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("HardwareIds")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         //creates and set the layout to activity_main
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //gain access to these views, set gatherData button to gatherData
-        val gatherData = findViewById<Button>(R.id.gatherData)
-        val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
-
-        //function to write data to file
-        fun fileWrite(){
-            //name the file after the device model
-            val fileName = """${Build.MODEL}.txt"""
-            //set content as the output of resultsTextView text box
-            val content = resultsTextView.text.toString()
-
-            //create a file to write to and write to the file
-            val fileOutputStream: FileOutputStream = openFileOutput(fileName, Context.MODE_PRIVATE)
-            fileOutputStream.write(content.toByteArray())
-
-            //notification that it has been saved to file
-            showToast("Saved to file")
+        //main function to call other functions
+        fun main(){
+            gatherData()
         }
-
-        //when gatherData button has been clicked
-        gatherData.setOnClickListener {
-            //hide the button
-            gatherData.visibility = View.GONE
-            //gather information of device
-            resultsTextView.text = ("BOARD: " + Build.BOARD + "\n"
-                    + "BOOTLOADER: " + Build.BOOTLOADER + "\n"
-                    + "BRAND: " + Build.BRAND + "\n"
-                    + "DEVICE: " + Build.DEVICE + "\n"
-                    + "DISPLAY: " + Build.DISPLAY + "\n"
-                    + "FINGERPRINT: " + Build.FINGERPRINT + "\n"
-                    + "HARDWARE: " + Build.HARDWARE + "\n"
-                    + "HOST: " + Build.HOST + "\n"
-                    + "ID: " + Build.ID + "\n"
-                    + "MANUFACTURER: " + Build.MANUFACTURER + "\n"
-                    + "MODEL: " + Build.MODEL + "\n"
-                    + "PRODUCT: " + Build.PRODUCT + "\n"
-                    + "SERIAL: " + Build.SERIAL + "\n"
-                    + "TAGS: " + Build.TAGS + "\n"
-                    + "USER: " + Build.TIME + "\n"
-                    + "USER: " + Build.USER + "\n")
-            //call fileWrite() to write to a file
-            fileWrite()
-        }
+        //call main function
+        main()
     }
+    //function to write data to file
+    fun fileWrite(resultsTextView:TextView){
+        //name the file after the device model
+        val fileName = """${Build.MODEL}.txt"""
+        //set content as the output of resultsTextView text box
+        val content = resultsTextView.text.toString()
+
+        //create a file to write to and write to the file
+        val fileOutputStream: FileOutputStream = openFileOutput(fileName, Context.MODE_PRIVATE)
+        fileOutputStream.write(content.toByteArray())
+
+        //notification that it has been saved to file
+        showToast("Saved to file")
+    }
+
+    //gathers data of device
+    fun gatherData(){
+        val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
+        resultsTextView.text = ("BOARD: " + Build.BOARD + "\n"
+                + "BOOTLOADER: " + Build.BOOTLOADER + "\n"
+                + "BRAND: " + Build.BRAND + "\n"
+                + "DEVICE: " + Build.DEVICE + "\n"
+                + "DISPLAY: " + Build.DISPLAY + "\n"
+                + "FINGERPRINT: " + Build.FINGERPRINT + "\n"
+                + "HARDWARE: " + Build.HARDWARE + "\n"
+                + "HOST: " + Build.HOST + "\n"
+                + "ID: " + Build.ID + "\n"
+                + "MANUFACTURER: " + Build.MANUFACTURER + "\n"
+                + "MODEL: " + Build.MODEL + "\n"
+                + "PRODUCT: " + Build.PRODUCT + "\n"
+                + "SERIAL: " + Build.SERIAL + "\n"
+                + "TAGS: " + Build.TAGS + "\n"
+                + "USER: " + Build.TIME + "\n"
+                + "USER: " + Build.USER + "\n")
+        //call fileWrite() to write to a file
+        fileWrite(resultsTextView)
+    }
+
     //notification pop up, extension function for Context
     fun Context.showToast(text: CharSequence, duration: Int = Toast.LENGTH_SHORT){
         Toast.makeText(this, text, duration).show()
